e3e192a44d897e60b9b86c3acc6f35e159da296e
youpy <youpy_yagi0311@yahoo.co.jp>
2016-08-27 07:43:09 +0000

    firstcommit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,47 @@
+class PicturesController < ApplicationController
+  before_action :set_picture, only: [:edit,:update,:destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(picture_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(picture_params)
+    # @picture.user_id = current_user.id
+    if @picture.save
+        redirect_to pictures_path, notice: "写真を投稿しました"
+    else
+      render action: 'new'
+    end
+  end
+
+
+  def edit
+  end
+
+  def update
+    @picture.update(picture_params)
+    redirect_to pictures_path, notice: "写真を更新しました"
+  end
+
+  def destroy
+    @picture.destroy
+    redirect_to pictures_path, notice: "写真を削除しました"
+  end
+
+  private
+    def picture_params
+      params.require(:picture).permit(:image)
+    end
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+end
